8f959897e41eb9d7687e1300e831bba5f2d8fb85
Nazar Vynnyk <n.vynnyk>
2023-07-19 12:17:27 +0200

    add new code 3

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,7 @@ FROM python:3.9
 WORKDIR /app
 RUN apt-get update && apt-get install -y git 
 RUN git clone https://github.com/Nzrvynnyk/tacktical_jenkins.git .
+RUN ls
 COPY requirements.txt .
 RUN pip install --upgrade pip
 RUN pip install -r requirements.txt
---
+++ userall.py
@@ -0,0 +1,29 @@
+import requests
+import json
+import sys
+
+API_KEY = sys.argv[1]
+
+API = "https://api.nzrb.fun"
+HEADERS = {
+    "Content-Type": "application/json",
+    "X-API-KEY": f"{API_KEY}",
+}
+
+def get_users():
+    users_url = f"{API}/agents/"
+    response = requests.get(users_url, headers=HEADERS)
+    print(response)
+    return response.json()
+
+def print_users_name():
+    users_data = get_users()
+    if users_data: 
+        for users in users_data:
+            users_name = users.get("logged_username")
+            users_host = users.get("hostname")
+            print(f"List of Users: {users_name}, and Hostname: {users_host}")
+    else: 
+        raise ValueError("No found")
+
+print_users_name()
